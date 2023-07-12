03b40adffde1fb226c62ec064be02fa68a871ea4
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-07-12 16:06:12 +0300

    solved

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,13 +1,37 @@
+import { selectorOfBreed, catInfoElement, errEl } from '.';
+
 function fetchBreeds(url) {
   return fetch(url)
     .then(r => r.json())
     .then(arrayOfObjects => {
-      // console.dir(arrayOfObjects);
+      console.dir(arrayOfObjects);
       return arrayOfObjects;
     })
-    .catch(er => console.log(er));
+    .catch(er => {
+      catInfoElement.innerHTML = '';
+      errEl.style.display = 'block';
+      console.log(er);
+    });
+}
+
+function fetchCatByBreed(breedId) {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  )
+    .then(r => {
+      return r.json();
+    })
+    .then(catImg => {
+      return catImg[0].url;
+    })
+    .catch(er => {
+      catInfoElement.innerHTML = '';
+
+      console.log(er);
+    });
 }
 
 export default {
   fetchBreeds,
+  fetchCatByBreed,
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import axios from 'axios';
 import CatAPI from './cat-api';
+import SlimSelect from 'slim-select';
+
+let select = null;
 
 axios.defaults.headers.common['x-api-key'] =
   'live_wmRji3fnJQHS4qHXv8dtXH1MgZnrctjtZdiNMlHltH53uuTHiUJOmM2azdaD3z8E';
 
-const selectorOfBreed = document.querySelector('.breed-select');
-
+export const selectorOfBreed = document.querySelector('.breed-select');
+export const catInfoElement = document.querySelector('.cat-info');
 const url = 'https://api.thecatapi.com/v1/breeds';
+const loaderEl = document.querySelector('.loader');
+export const errEl = document.querySelector('.error');
+
+loaderEl.style.display = 'none';
+errEl.style.display = 'none';
 
 function renderOptions() {
   CatAPI.fetchBreeds(url).then(data => {
@@ -16,8 +24,59 @@ function renderOptions() {
       })
       .join('');
     selectorOfBreed.insertAdjacentHTML('afterbegin', res);
+    select = new SlimSelect({
+      select: '.breed-select',
+      events: {
+        afterChange: newVal => {
+          loaderEl.style.display = 'block';
+
+          console.log(newVal);
+          const id = newVal[0].value;
+          console.log(id);
+          console.log(this);
+          CatAPI.fetchCatByBreed(id).then(data => {
+            console.log(data);
+            const imgElement = `<img src="${data}" alt="Citty" width = '500'>`;
+            catInfoElement.innerHTML = imgElement;
+          });
+
+          CatAPI.fetchBreeds(url)
+            .then(massivKotov => {
+              const cat = massivKotov.find(cat => cat.id === id);
+              console.log(cat);
+              return `<p>${cat.name}</p> <p>${cat.description}</p> <p>${cat.temperament}</p>`;
+            })
+            .then(markup => {
+              catInfoElement.insertAdjacentHTML('afterbegin', markup);
+              loaderEl.style.display = 'none';
+            });
+        },
+      },
+    });
   });
 }
+
 renderOptions();
+
+function onSelectorChange({ target }) {
+  // const id = target.value;
+  // console.log(id);
+  // CatAPI.fetchCatByBreed(id).then(data => {
+  //   console.log(data);
+  //   const imgElement = `<img src="${data}" alt="Citty" width = '500'>`;
+  //   catInfoElement.innerHTML = imgElement;
+  // });
+  // CatAPI.fetchBreeds(url)
+  //   .then(massivKotov => {
+  //     const cat = massivKotov.find(cat => cat.id === id);
+  //     console.log(cat);
+  //     return `<p>${cat.name}</p> <p>${cat.description}</p> <p>${cat.temperament}</p>`;
+  //   })
+  //   .then(markup => {
+  //     catInfoElement.insertAdjacentHTML('afterbegin', markup);
+  //   });
+}
+
+// selectorOfBreed.addEventListener('change', onSelectorChange);
 
 /* <option value="1"> Miner </option>; */
